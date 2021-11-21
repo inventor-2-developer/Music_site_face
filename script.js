187229771ff321c352e5edfbf32a2d2f75d6d260
inventor-2-developer <94792982+inventor-2-developer@users.noreply.github.com>
2021-11-21 09:40:08 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('label1')object.addEventListener("mouseover" function())
+{document.getElementById('label1').style.color="green";});
+
+document.getElementById('label1')object.addEventListener("onmouseout" function ()) {
+  document.getElementById('label1').style.color="red";
+
+});
