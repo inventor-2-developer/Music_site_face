ad53e61dbb73a1d59d9e47bf2e79e2d66f1a3189
Frank King <94792982+inventor-2-developer@users.noreply.github.com>
2021-11-21 19:20:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-document.getElementById('label1')object.addEventListener("mouseover" function())
-{document.getElementById('label1').style.color="green";});
-
-document.getElementById('label1')object.addEventListener("onmouseout" function ()) {
-  document.getElementById('label1').style.color="red";
-
-});
+JS very soon!!!!!
